ee65797ae823d25cbf5de70dbd28d02b08ecf9dc
tokenit1331-creator <token.it1331@gmail.com>
2026-04-24 20:05:39 +0300

    📚 Урок enumerate: main.rs

---
+++ 09_enumerate/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("📚 Урок: enumerate() в Rust");
+    println!();
+    println!("Запусти один из примеров:");
+    println!("  cargo run --example 01_basic_enumerate");
+    println!("  cargo run --example 02_enumerate_with_crypto");
+    println!("  cargo run --example 03_enumerate_filter");
+    println!("  cargo run --example 04_enumerate_collect");
+    println!("  cargo run --example exercise");
+    println!();
+    println!("Или запусти тесты:");
+    println!("  cargo test");
+}
